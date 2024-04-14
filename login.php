6658008fea518176a87771d5a2e2d6a90bcca122
mohammadelendil <mohammadapp1989@gmail.com>
2024-04-14 22:30:33 +0330

    L1.2-Send-Resume

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>send resume</title>
+</head>
+<body>
+    <h1>Send Resume</h1>
+    <P>PLEASE SEND YOUR RESUME</P>
+    
+    <form action="login-store.php" method="post" enctype="multipart/form-data">
+
+    <br>
+    <br>
+    <input type="file" name="resume">
+    <br>
+    <br>
+    <button type="submit">submit</button>
+</form>
+</body>
+</html>
